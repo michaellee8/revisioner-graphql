e3fde629c8a43e1abbf057843e2415a639925bc8
Michael Lee <ckmichael8@gmail.com>
2017-09-04 05:33:36 +0800

    Implement a consistent series db init script

--- src/initDb.js
+++ src/initDb.js
@@ -1,4 +1,5 @@
 var Sequelize = require("sequelize");
+var Promise = require("bluebird");
 
 const db = new Sequelize("revisioner", "root", "mlml1026", {
   host: "localhost",
@@ -6,18 +7,34 @@ const db = new Sequelize("revisioner", "root", "mlml1026", {
 });
 
 const models = require("./models")(db);
+
+function getModelSyncPromise(models, keys, index = 0) {
+  return models[keys[index]]
+    .sync({ force: true, logging: false })
+    .then(() => {
+      console.log(
+        "\x1b[32m" + "Model " + keys[index] + " has been initalized" + "\x1b[0m"
+      );
+      return index + 1 < keys.length
+        ? getModelSyncPromise(models, keys, index + 1)
+        : setTimeout(process.exit, 3000, 0);
+    })
+    .catch(err =>
+      console.log(
+        "\x1b[31m" +
+          "Model " +
+          keys[index] +
+          " has not been initalized due to an error",
+        err,
+        "\x1b[0m"
+      )
+    );
+}
 db
   .authenticate()
   .then(() => {
     console.log("Connection has been established successfully.");
-    Object.keys(models).forEach(key =>
-      models[key]
-        .sync({ force: true })
-        .then(() => console.log("Model " + key + " is initalized"))
-        .catch(err =>
-          console.log("Model " + key + " is not initalized because of:\n", err)
-        )
-    );
+    getModelSyncPromise(models, Object.keys(models));
   })
   .catch(err => {
     console.error("Unable to connect to the database:", err);
