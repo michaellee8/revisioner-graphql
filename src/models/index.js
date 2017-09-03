d16c953f21c287bee59370fbea5363925b7a76d2
Michael Lee <ckmichael8@gmail.com>
2017-09-04 05:33:53 +0800

    Define new relationships

--- src/models/index.js
+++ src/models/index.js
@@ -1,54 +1,54 @@
 module.exports = sequelize => {
   var models = {
+    User: sequelize.import(__dirname + "/users"),
+    QuestionSet: sequelize.import(__dirname + "/questionSets"),
+    Question: sequelize.import(__dirname + "/questions"),
     QuestionAnswer: sequelize.import(__dirname + "/questionAnswers"),
     QuestionComment: sequelize.import(__dirname + "/questionComments"),
-    QuestionReaction: sequelize.import(__dirname + "/questionReactions"),
     QuestionReactionType: sequelize.import(
       __dirname + "/questionReactionTypes"
     ),
-    Question: sequelize.import(__dirname + "/questions"),
+    QuestionReaction: sequelize.import(__dirname + "/questionReactions"),
     QuestionSetComment: sequelize.import(__dirname + "/questionSetComments"),
     QuestionSetFollow: sequelize.import(__dirname + "/questionSetFollows"),
-    QuestionSetReaction: sequelize.import(__dirname + "/questionSetReactions"),
     QuestionSetReactionType: sequelize.import(
       __dirname + "/questionSetReactionTypes"
     ),
-    QuestionSet: sequelize.import(__dirname + "/questionSets"),
+    QuestionSetReaction: sequelize.import(__dirname + "/questionSetReactions"),
     QuestionSetTag: sequelize.import(__dirname + "/questionSetTags"),
     QuestionSumbit: sequelize.import(__dirname + "/questionSumbits"),
     QuestionTag: sequelize.import(__dirname + "/questionTags"),
-    User: sequelize.import(__dirname + "/users"),
     UserTag: sequelize.import(__dirname + "/userTags")
   };
 
-  models.Question.hasMany(models.QuestionComment, {
-    as: "questionComments",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
+  models.User.hasMany(models.QuestionSet, {
+    as: "questionSets",
+    foreignKey: "userId",
+    sourceKey: "userId"
   });
 
-  models.Question.hasMany(models.QuestionAnswer, {
-    as: "questionAnswers",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
+  models.User.hasMany(models.UserTag, {
+    as: "userTags",
+    foreignKey: "userId",
+    sourceKey: "userId"
   });
 
-  models.Question.hasMany(models.QuestionReaction, {
-    as: "questionReactions",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
+  models.User.hasMany(models.QuestionSetFollow, {
+    as: "questionSetFollows",
+    foreignKey: "userId",
+    sourceKey: "userId"
   });
 
-  models.Question.hasMany(models.QuestionTag, {
-    as: "questionTags",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
-  });
-
-  models.Question.hasMany(models.QuestionSumbit, {
+  models.User.hasMany(models.QuestionSumbit, {
     as: "questionSumbits",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
+    foreignKey: "userId",
+    sourceKey: "userId"
+  });
+
+  models.QuestionSet.hasMany(models.Question, {
+    as: "questions",
+    foreignKey: "questionSetId",
+    sourceKey: "questionSetId"
   });
 
   models.QuestionSet.hasMany(models.QuestionSetComment, {
@@ -75,34 +75,143 @@ module.exports = sequelize => {
     sourceKey: "questionSetId"
   });
 
-  models.User.hasMany(models.UserTag, {
-    as: "userTags",
+  models.QuestionSet.belongsTo(models.User, {
+    as: "user",
     foreignKey: "userId",
-    sourceKey: "userId"
+    targetKey: "userId"
   });
 
-  models.User.hasMany(models.QuestionSet, {
-    as: "questionSets",
-    foreignKey: "userId",
-    sourceKey: "userId"
-  });
-
-  models.UserTag.hasMany(models.User, {
-    as: "users",
-    foreignKey: "userId",
-    sourceKey: "userId"
-  });
-
-  models.QuestionTag.hasMany(models.Question, {
-    as: "questions",
+  models.Question.hasMany(models.QuestionAnswer, {
+    as: "questionAnswers",
     foreignKey: "questionId",
     sourceKey: "questionId"
   });
 
-  models.QuestionSetTag.hasMany(models.QuestionSet, {
-    as: "questionSets",
-    foreignKey: "questionSetId",
-    sourceKey: "questionSetId"
+  models.Question.hasMany(models.QuestionComment, {
+    as: "questionComments",
+    foreignKey: "questionId",
+    sourceKey: "questionId"
   });
+
+  models.Question.hasMany(models.QuestionReaction, {
+    as: "questionReactions",
+    foreignKey: "questionId",
+    sourceKey: "questionId"
+  });
+
+  models.Question.hasMany(models.QuestionTag, {
+    as: "questionTags",
+    foreignKey: "questionId",
+    sourceKey: "questionId"
+  });
+
+  models.Question.belongsTo(models.QuestionSet, {
+    as: "questionSet",
+    foreignKey: "questionSetId",
+    targetKey: "questionSetId"
+  });
+
+  models.QuestionAnswer.belongsTo(models.Question, {
+    as: "question",
+    foreignKey: "questionId",
+    targetKey: "questionId"
+  });
+
+  models.QuestionAnswer.hasMany(models.QuestionSumbit, {
+    as: "questionSumbits",
+    foreignKey: "questionAnswerId",
+    sourceKey: "questionAnswerId"
+  });
+
+  models.QuestionComment.belongsTo(models.Question, {
+    as: "question",
+    foreignKey: "questionId",
+    targetKey: "questionId"
+  });
+
+  models.QuestionComment.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionReaction.belongsTo(models.Question, {
+    as: "question",
+    foreignKey: "questionId",
+    targetKey: "questionId"
+  });
+
+  models.QuestionReaction.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionTag.belongsTo(models.Question, {
+    as: "question",
+    foreignKey: "questionId",
+    targetKey: "questionId"
+  });
+
+  models.QuestionSumbit.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionSumbit.belongsTo(models.QuestionAnswer, {
+    as: "questionAnswer",
+    foreignKey: "questionAnswerId",
+    targetKey: "questionAnswerId"
+  });
+
+  models.QuestionSetComment.belongsTo(models.QuestionSet, {
+    as: "questionSet",
+    foreignKey: "questionSetId",
+    targetKey: "questionSetId"
+  });
+
+  models.QuestionSetComment.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionSetFollow.belongsTo(models.QuestionSet, {
+    as: "questionSet",
+    foreignKey: "questionSetId",
+    targetKey: "questionSetId"
+  });
+
+  models.QuestionSetFollow.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionSetReaction.belongsTo(models.QuestionSet, {
+    as: "questionSet",
+    foreignKey: "questionSetId",
+    targetKey: "questionSetId"
+  });
+
+  models.QuestionSetReaction.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionSetTag.belongsTo(models.QuestionSet, {
+    as: "questionSet",
+    foreignKey: "questionSetId",
+    targetKey: "questionSetId"
+  });
+
+  models.QuestionSetTag.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
   return models;
 };
