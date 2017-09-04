30de042fb77542be1f8c8546b92261979c2197cb
Michael Lee <ckmichael8@gmail.com>
2017-09-04 16:12:13 +0800

    Assign names to relationships

--- src/models/index.js
+++ src/models/index.js
@@ -21,193 +21,256 @@ module.exports = sequelize => {
     UserTag: sequelize.import(__dirname + "/userTags")
   };
 
-  models.User.hasMany(models.QuestionSet, {
+  models.User.QuestionSet = models.User.hasMany(models.QuestionSet, {
     as: "questionSets",
     foreignKey: "userId",
     sourceKey: "userId"
   });
 
-  models.User.hasMany(models.UserTag, {
+  models.User.UserTag = models.User.hasMany(models.UserTag, {
     as: "userTags",
     foreignKey: "userId",
     sourceKey: "userId"
   });
 
-  models.User.hasMany(models.QuestionSetFollow, {
-    as: "questionSetFollows",
-    foreignKey: "userId",
-    sourceKey: "userId"
-  });
+  models.User.QuestionSetFollow = models.User.hasMany(
+    models.QuestionSetFollow,
+    {
+      as: "questionSetFollows",
+      foreignKey: "userId",
+      sourceKey: "userId"
+    }
+  );
 
-  models.User.hasMany(models.QuestionSumbit, {
+  models.User.QuestionSumbit = models.User.hasMany(models.QuestionSumbit, {
     as: "questionSumbits",
     foreignKey: "userId",
     sourceKey: "userId"
   });
 
-  models.QuestionSet.hasMany(models.Question, {
+  models.QuestionSet.Question = models.QuestionSet.hasMany(models.Question, {
     as: "questions",
     foreignKey: "questionSetId",
     sourceKey: "questionSetId"
   });
 
-  models.QuestionSet.hasMany(models.QuestionSetComment, {
-    as: "questionSetComments",
-    foreignKey: "questionSetId",
-    sourceKey: "questionSetId"
-  });
+  models.QuestionSet.QuestionSetComment = models.QuestionSet.hasMany(
+    models.QuestionSetComment,
+    {
+      as: "questionSetComments",
+      foreignKey: "questionSetId",
+      sourceKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSet.hasMany(models.QuestionSetFollow, {
-    as: "questionSetFollows",
-    foreignKey: "questionSetId",
-    sourceKey: "questionSetId"
-  });
+  models.QuestionSet.QuestionSetFollow = models.QuestionSet.hasMany(
+    models.QuestionSetFollow,
+    {
+      as: "questionSetFollows",
+      foreignKey: "questionSetId",
+      sourceKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSet.hasMany(models.QuestionSetReaction, {
-    as: "questionSetReactions",
-    foreignKey: "questionSetId",
-    sourceKey: "questionSetId"
-  });
+  models.QuestionSet.QuestionSetReaction = models.QuestionSet.hasMany(
+    models.QuestionSetReaction,
+    {
+      as: "questionSetReactions",
+      foreignKey: "questionSetId",
+      sourceKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSet.hasMany(models.QuestionSetTag, {
-    as: "questionSetTags",
-    foreignKey: "questionSetId",
-    sourceKey: "questionSetId"
-  });
+  models.QuestionSet.QuestionSetTag = models.QuestionSet.hasMany(
+    models.QuestionSetTag,
+    {
+      as: "questionSetTags",
+      foreignKey: "questionSetId",
+      sourceKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSet.belongsTo(models.User, {
+  models.QuestionSet.User = models.QuestionSet.belongsTo(models.User, {
     as: "user",
     foreignKey: "userId",
     targetKey: "userId"
   });
 
-  models.Question.hasMany(models.QuestionAnswer, {
-    as: "questionAnswers",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
-  });
+  models.Question.QuestionAnswer = models.Question.hasMany(
+    models.QuestionAnswer,
+    {
+      as: "questionAnswers",
+      foreignKey: "questionId",
+      sourceKey: "questionId"
+    }
+  );
 
-  models.Question.hasMany(models.QuestionComment, {
-    as: "questionComments",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
-  });
+  models.Question.QuestionComment = models.Question.hasMany(
+    models.QuestionComment,
+    {
+      as: "questionComments",
+      foreignKey: "questionId",
+      sourceKey: "questionId"
+    }
+  );
 
-  models.Question.hasMany(models.QuestionReaction, {
-    as: "questionReactions",
-    foreignKey: "questionId",
-    sourceKey: "questionId"
-  });
+  models.Question.QuestionReaction = models.Question.hasMany(
+    models.QuestionReaction,
+    {
+      as: "questionReactions",
+      foreignKey: "questionId",
+      sourceKey: "questionId"
+    }
+  );
 
-  models.Question.hasMany(models.QuestionTag, {
+  models.Question.QuestionTag = models.Question.hasMany(models.QuestionTag, {
     as: "questionTags",
     foreignKey: "questionId",
     sourceKey: "questionId"
   });
 
-  models.Question.belongsTo(models.QuestionSet, {
+  models.Question.QuestionSet = models.Question.belongsTo(models.QuestionSet, {
     as: "questionSet",
     foreignKey: "questionSetId",
     targetKey: "questionSetId"
   });
 
-  models.QuestionAnswer.belongsTo(models.Question, {
+  models.QuestionAnswer.Question = models.QuestionAnswer.belongsTo(
+    models.Question,
+    {
+      as: "question",
+      foreignKey: "questionId",
+      targetKey: "questionId"
+    }
+  );
+
+  models.QuestionAnswer.QuestionSumbit = models.QuestionAnswer.hasMany(
+    models.QuestionSumbit,
+    {
+      as: "questionSumbits",
+      foreignKey: "questionAnswerId",
+      sourceKey: "questionAnswerId"
+    }
+  );
+
+  models.QuestionComment.Question = models.QuestionComment.belongsTo(
+    models.Question,
+    {
+      as: "question",
+      foreignKey: "questionId",
+      targetKey: "questionId"
+    }
+  );
+
+  models.QuestionComment.User = models.QuestionComment.belongsTo(models.User, {
+    as: "user",
+    foreignKey: "userId",
+    targetKey: "userId"
+  });
+
+  models.QuestionReaction.Question = models.QuestionReaction.belongsTo(
+    models.Question,
+    {
+      as: "question",
+      foreignKey: "questionId",
+      targetKey: "questionId"
+    }
+  );
+
+  models.QuestionReaction.User = models.QuestionReaction.belongsTo(
+    models.User,
+    {
+      as: "user",
+      foreignKey: "userId",
+      targetKey: "userId"
+    }
+  );
+
+  models.QuestionTag.Question = models.QuestionTag.belongsTo(models.Question, {
     as: "question",
     foreignKey: "questionId",
     targetKey: "questionId"
   });
 
-  models.QuestionAnswer.hasMany(models.QuestionSumbit, {
-    as: "questionSumbits",
-    foreignKey: "questionAnswerId",
-    sourceKey: "questionAnswerId"
-  });
-
-  models.QuestionComment.belongsTo(models.Question, {
-    as: "question",
-    foreignKey: "questionId",
-    targetKey: "questionId"
-  });
-
-  models.QuestionComment.belongsTo(models.User, {
+  models.QuestionSumbit.User = models.QuestionSumbit.belongsTo(models.User, {
     as: "user",
     foreignKey: "userId",
     targetKey: "userId"
   });
 
-  models.QuestionReaction.belongsTo(models.Question, {
-    as: "question",
-    foreignKey: "questionId",
-    targetKey: "questionId"
-  });
+  models.QuestionSumbit.QuestionAnswer = models.QuestionSumbit.belongsTo(
+    models.QuestionAnswer,
+    {
+      as: "questionAnswer",
+      foreignKey: "questionAnswerId",
+      targetKey: "questionAnswerId"
+    }
+  );
 
-  models.QuestionReaction.belongsTo(models.User, {
-    as: "user",
-    foreignKey: "userId",
-    targetKey: "userId"
-  });
+  models.QuestionSetComment.QuestionSet = models.QuestionSetComment.belongsTo(
+    models.QuestionSet,
+    {
+      as: "questionSet",
+      foreignKey: "questionSetId",
+      targetKey: "questionSetId"
+    }
+  );
 
-  models.QuestionTag.belongsTo(models.Question, {
-    as: "question",
-    foreignKey: "questionId",
-    targetKey: "questionId"
-  });
+  models.QuestionSetComment.User = models.QuestionSetComment.belongsTo(
+    models.User,
+    {
+      as: "user",
+      foreignKey: "userId",
+      targetKey: "userId"
+    }
+  );
 
-  models.QuestionSumbit.belongsTo(models.User, {
-    as: "user",
-    foreignKey: "userId",
-    targetKey: "userId"
-  });
+  models.QuestionSetFollow.QuestionSet = models.QuestionSetFollow.belongsTo(
+    models.QuestionSet,
+    {
+      as: "questionSet",
+      foreignKey: "questionSetId",
+      targetKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSumbit.belongsTo(models.QuestionAnswer, {
-    as: "questionAnswer",
-    foreignKey: "questionAnswerId",
-    targetKey: "questionAnswerId"
-  });
+  models.QuestionSetFollow.User = models.QuestionSetFollow.belongsTo(
+    models.User,
+    {
+      as: "user",
+      foreignKey: "userId",
+      targetKey: "userId"
+    }
+  );
 
-  models.QuestionSetComment.belongsTo(models.QuestionSet, {
-    as: "questionSet",
-    foreignKey: "questionSetId",
-    targetKey: "questionSetId"
-  });
+  models.QuestionSetReaction.QuestionSet = models.QuestionSetReaction.belongsTo(
+    models.QuestionSet,
+    {
+      as: "questionSet",
+      foreignKey: "questionSetId",
+      targetKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSetComment.belongsTo(models.User, {
-    as: "user",
-    foreignKey: "userId",
-    targetKey: "userId"
-  });
+  models.QuestionSetReaction.User = models.QuestionSetReaction.belongsTo(
+    models.User,
+    {
+      as: "user",
+      foreignKey: "userId",
+      targetKey: "userId"
+    }
+  );
 
-  models.QuestionSetFollow.belongsTo(models.QuestionSet, {
-    as: "questionSet",
-    foreignKey: "questionSetId",
-    targetKey: "questionSetId"
-  });
+  models.QuestionSetTag.QuestionSet = models.QuestionSetTag.belongsTo(
+    models.QuestionSet,
+    {
+      as: "questionSet",
+      foreignKey: "questionSetId",
+      targetKey: "questionSetId"
+    }
+  );
 
-  models.QuestionSetFollow.belongsTo(models.User, {
-    as: "user",
-    foreignKey: "userId",
-    targetKey: "userId"
-  });
-
-  models.QuestionSetReaction.belongsTo(models.QuestionSet, {
-    as: "questionSet",
-    foreignKey: "questionSetId",
-    targetKey: "questionSetId"
-  });
-
-  models.QuestionSetReaction.belongsTo(models.User, {
-    as: "user",
-    foreignKey: "userId",
-    targetKey: "userId"
-  });
-
-  models.QuestionSetTag.belongsTo(models.QuestionSet, {
-    as: "questionSet",
-    foreignKey: "questionSetId",
-    targetKey: "questionSetId"
-  });
-
-  models.UserTag.belongsTo(models.User, {
+  models.UserTag.User = models.UserTag.belongsTo(models.User, {
     as: "user",
     foreignKey: "userId",
     targetKey: "userId"
